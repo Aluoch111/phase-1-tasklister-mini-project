b2a8a7817f0d9f546b20000e534c53cef9f7a803
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-09-06 12:26:27 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", (e) => {
+        e.preventDefault();
+
+        const form = document.getElementById("create-task-form");
+        form.addEventListener("submit", (e) => {
+
+            e.preventDefault();
+            const task = document.getElementById("new-task-description");
+		const list = document.getElementById("tasks");
+		const newItem = document.createElement("li");
+		newItem.innerText = task.value;
+		list.appendChild(newItem);
+		// Reset the dom
+		e.target.reset();
+        });
+
 });
+
+
